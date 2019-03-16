508ac98a0db54d0ce47275037dbc20d4d3100812
5rietz <5rietz@informatik.uni-hamburg.de>
2019-03-16 01:57:37 +0100

    man im getting tired

--- h_opt_dsst.py
+++ h_opt_dsst.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
     if args.gpu is None:
         args.gpu = 0
 
-    test = True
+    test = False
 
     change_parameter(parameter_name='scale_factor', additional_parameters=[
         ["use_scale_estimation", True],
