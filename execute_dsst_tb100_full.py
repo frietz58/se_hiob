7b5df890fbffd1cad2bacf8c2225f4a70d7ce119
Middy <5rietz@informatik.uni-hamburg.de>
2019-03-20 02:20:00 +0100

    cand dsst full experiment setting

---
+++ execute_dsst_tb100_full.py
@@ -0,0 +1,25 @@
+import subprocess
+import ruamel.yaml
+import gc
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Execute Parameter Optimization')
+parser.add_argument('-g', '--gpu')
+parser.add_argument('-e', '--environment')
+parser.add_argument('-t', '--tracker')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    if args.gpu is None:
+        args.gpu = 0
+
+    test = False
+
+    for i in range(0, 3):
+        print("execution  " + str(i))
+
+        if not test:
+            subprocess.call(['python', 'hiob_cli.py', '-e', 'config/environment_experiments_dsst.yaml', '-t', 'config/tracker_dsst.yaml', '-g', str(args.gpu)])
